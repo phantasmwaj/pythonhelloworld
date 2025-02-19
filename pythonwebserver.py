4c2123600d5ada61c8bb6c22453dd7402a5c2a6b
Waj <wajsampathkumar@gmail.com>
2025-02-18 20:53:47 -0800

    print today's date

--- pythonwebserver.py
+++ pythonwebserver.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def welcome():
-    return "Welcome to the Python Web Server!"
+    return "Welcome to the Python Web Server! - today's date {#datetime.datetime.now()}"
 
 
 if (__name__ == "__main__"):
