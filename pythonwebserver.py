82289029b7c7f68435d83975415cc47f4c7037cd
Waj <wajsampathkumar@gmail.com>
2025-02-18 20:58:49 -0800

    fixed datetime bug1

--- pythonwebserver.py
+++ pythonwebserver.py
@@ -1,10 +1,12 @@
+import datetime
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def welcome():
-    return "Welcome to the Python Web Server! - today's date {#datetime.datetime.now()}"
+    
+    return "Welcome to the Python Web Server! - today's date " +  datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 if (__name__ == "__main__"):
