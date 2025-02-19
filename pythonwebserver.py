d7a75817994d1a41057cb476e26caceba61a742a
Waj <wajsampathkumar@gmail.com>
2025-02-18 21:07:14 -0800

    get endpoint to list students

--- pythonwebserver.py
+++ pythonwebserver.py
@@ -3,11 +3,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+studentsDb = [
+    {"id": 1, "name": "John", "age": 20},
+    {"id": 2, "name": "Jane", "age": 22},
+    {"id": 3, "name": "Joe", "age": 21}
+]
+
 @app.route("/", methods=["GET"])
 def welcome():
-    
     return "Welcome to the Python Web Server! - today's date " +  datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+@app.route("/students", methods=["GET"])
+def get_students():
+    return {"students": studentsDb}
 
 if (__name__ == "__main__"):
     app.run()
